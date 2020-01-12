0cc299c636377855856527c5410131ed44aaa804
jonleesmith <jonleesmith@me.com>
2020-01-12 20:43:39 +0000

    Netlify function

--- functions/meta.js
+++ functions/meta.js
@@ -4,17 +4,17 @@ const API_ENDPOINT = "https://icanhazdadjoke.com/";
 
 exports.handler = async (event, context) => {
     return fetch(API_ENDPOINT, {
-            headers: {
-                "Accept": "application/json"
-            }
-        })
-        .then(response => response.json())
-        .then(data => ({
-            statusCode: 200,
-            body: data.joke
-        }))
-        .catch(error => ({
-            statusCode: 422,
-            body: String(error)
-        }));
+        headers: {
+            "Accept": "application/json"
+        }
+    })
+    .then(response => response.json())
+    .then(data => ({
+        statusCode: 200,
+        body: data.joke
+    }))
+    .catch(error => ({
+        statusCode: 422,
+        body: String(error)
+    }));
 };
