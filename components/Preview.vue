60c95c798e82b30193968010097cfd0ea8a35fc1
jonleesmith <jonleesmith@me.com>
2020-01-12 20:47:26 +0000

    Revert to page-metadata-parser

--- components/Preview.vue
+++ components/Preview.vue
@@ -8,20 +8,6 @@
     import { getMetadata } from 'page-metadata-parser';
     import domino from 'domino'
 
-    const metascraper = require('metascraper')([
-        require('metascraper-author')(),
-        require('metascraper-date')(),
-        require('metascraper-description')(),
-        require('metascraper-image')(),
-        require('metascraper-logo')(),
-        require('metascraper-clearbit')(),
-        require('metascraper-publisher')(),
-        require('metascraper-title')(),
-        require('metascraper-url')()
-    ])
-
-const got = require('got')
-
     export default {
 
         props: ['url'],
@@ -33,16 +19,12 @@ const got = require('got')
         },
 
         async mounted() {
-            // const url = this.url;
-            // const response = await fetch(url);
-            // const html = await response.text();
-            // const doc = domino.createWindow(html).document;
-            // this.meta = getMetadata(doc, url);
-            // this.$emit('complete', this.meta)
-
-            const { body: html, url } = await got(this.url)
-            this.meta = await metascraper({ html, url })
-            console.log(this.meta)
+            const url = this.url;
+            const response = await fetch(url);
+            const html = await response.text();
+            const doc = domino.createWindow(html).document;
+            this.meta = getMetadata(doc, url);
+            this.$emit('complete', this.meta)
         }
 
     }
