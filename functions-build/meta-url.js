65472c62a86bd7a47a980f1a6d272cfa42e22212
jonleesmith <jonleesmith@me.com>
2020-01-28 13:18:13 +0000

    Compile functions for build

--- functions-build/meta-url.js
+++ functions-build/meta-url.js
@@ -7,7 +7,6 @@ exports.handler = async function(event, context) {
     const url = event.queryStringParameters.url
     return axios.get(url)
     .then(async (response) => {
-        console.log(response)
         const html = response.data
         const doc = domino.createWindow(html).document;
         const meta = getMetadata(doc, url);
@@ -17,7 +16,6 @@ exports.handler = async function(event, context) {
         }
     })
     .catch((err) => {
-        console.log(err)
         return {
             statusCode: 500,
             body: JSON.stringify(err.message)
