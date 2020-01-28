90a7054bc4c8defa723b715c0a7308099f757017
jonleesmith <jonleesmith@me.com>
2020-01-28 16:34:58 +0000

    alternative function

--- functions-build/meta-url.js
+++ functions-build/meta-url.js
@@ -1,18 +1,22 @@
 /* eslint-disable */
-const { getMetadata } = require('page-metadata-parser');
+const meta = require('page-metadata-parser');
 const domino = require('domino')
 const axios = require('axios')
 
 exports.handler = async function(event, context) {
     const url = event.queryStringParameters.url
     return axios.get(url)
-    .then(async (response) => {
+    .then((response) => {
         const html = response.data
-        const doc = domino.createWindow(html).document;
-        const meta = getMetadata(doc, url);
+        console.log(typeof html)
+        const window = domino.createWindow({});
+        console.log(window)
+        // const meta = getMetadata(window.document, url);
         return {
             statusCode: 200,
-            body: JSON.stringify(meta)
+            body: JSON.stringify({
+
+            })
         }
     })
     .catch((err) => {
